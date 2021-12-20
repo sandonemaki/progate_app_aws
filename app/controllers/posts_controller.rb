23d974a9cc04fee606dcc52ed29d4d64b8337216
vagrant <vagrant@localhost.localdomain>
2021-12-20 19:09:49 +0000

    投稿詳細ページを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,9 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(created_at: :desc)
   end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
 end
