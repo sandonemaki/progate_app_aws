831fb7cefd8b3188e381f4e80cb93f6eed265e30
vagrant <vagrant@localhost.localdomain>
2021-12-20 13:45:17 +0000

    ルートurlを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/top'
-  get 'makihome/makitop'
+  root 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
