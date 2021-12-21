bc5a0d65d375427611d8aa20eaa902910ece3d0c
vagrant <vagrant@localhost.localdomain>
2021-12-21 04:17:41 +0000

    新規投稿フォームを作成して保存

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,9 @@ class PostsController < ApplicationController
   end
 
   def new
+  end
+  
+  def create
     @post = Post.new(content: params[:content])
     @post.save
     redirect_to("/posts/index")
