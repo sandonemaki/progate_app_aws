3c7019dfa46a86a54764703ba101c0aaee9e009b
maki <work@kamumaki.net>
2021-12-22 11:37:34 +0000

    自分の情報のみ編集できるようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
   before_action :forbid_login_user, {only: [:new, :login_form, :create, :login]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
 
   def index
     @users = User.all
@@ -80,6 +81,12 @@ class UsersController < ApplicationController
     redirect_to("/login")
   end
 
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
 
 
 
